fef1e91a5e75c6231363cf29f4ff24979a747a03
lpfan <misha.shchetinin@gmail.com>
2017-05-04 18:12:16 +0300

    added portfolio component

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -2,6 +2,7 @@
   <div>
       <ul class="nav nav-pills">
         <li role="presentation" class="active"><router-link to="/">Home</router-link></li>
+        <li role="presentation"><router-link to="/portfolio">Portfolio</router-link></li>
         <li role="presentation"><router-link to="/about">About</router-link></li>
       </ul>
   </div>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,7 +1,17 @@
 import About from './components/About.vue';
 import Home from './components/Home.vue';
+import Portfolio from './components/Portfolio.vue';
 
-export const routes = [
-    { path: '/', component: Home },
-    { path: '/about', component: About}
+export const routes = [{
+		path: '/',
+		component: Home
+	},
+	{
+		path: '/about',
+		component: About
+	},
+	{
+		path: '/portfolio',
+		component: Portfolio
+	}
 ];
