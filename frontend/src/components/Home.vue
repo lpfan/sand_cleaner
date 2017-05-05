7d1653890e2f4202625a4f42691a7e533a173acb
lpfan <misha.shchetinin@gmail.com>
2017-05-05 18:21:39 +0300

    added email field :)

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
-    Home Component
+  <div class="row">
+      <div class="col-xs-4 col-md-4">
+          <span>Client Info</span>
+          <form class="">
+              <div class="form-group">
+                  <label for="email">Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    class="form-control"
+                    v-model="email">
+              </div>
+          </form>
+      </div>
+      <div class="col-xs-4 col-md-4">
+          Order Info
+      </div>
+      <div class="col-xs-4 col-md-4">
+          Submit
+      </div>
   </div>
 </template>
 <script>
     export default {
-        data: function data() {
+        data() {
             return {
-
+                email: ''
             }
         }
     }
